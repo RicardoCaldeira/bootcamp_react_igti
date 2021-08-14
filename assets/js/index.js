edb21e1deb5d6b09b331e4958fd7aa761c32df6b
Ricardo Caldeira <ricardocaldeira.zetta@ufla.br>
2021-08-14 12:52:04 -0300

    :construction: HOME - Construindo grafico e barras

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,18 +20,23 @@ function loadKpis(kpis) {
 	totalRecuperados.innerText = kpis.Global.TotalRecovered.toLocaleString("PT");
 
 	loadPizza(kpis);
-	
+	loadBars(kpis);
+
 }
 
 function loadPizza(kpis) {
 	var pizza = document.getElementById('pizza');
-	var myChart = new Chart(pizza, {
+	var myPieChart = new Chart(pizza, {
 		type: 'pie',
 		data: {
 			labels: ['Confirmados', 'Recuperados', 'Mortes'],
 			datasets: [{
 				label: '# of Votes',
-				data: [12, 19, 3],
+				data: [
+					kpis.Global.TotalConfirmed,
+					kpis.Global.TotalRecovered,
+					kpis.Global.TotalDeaths
+				],
 				backgroundColor: [
 					'rgba(255, 99, 132)',//red
 					'rgba(54, 162, 235)',//blue
@@ -54,6 +59,57 @@ function loadPizza(kpis) {
 				title: {
 					display: true,
 					text: "Distribuição de novos casos"
+				}
+			}
+		}
+	});
+}
+
+function loadBars(kpis) {
+
+	let top10 = [];
+
+	for (let i = 0; i < 10; i++) {
+		let pos = kpis.Countries.indexOf( _.maxBy(kpis.Countries, (c)=> {return c.TotalDeaths}));
+		top10[i] = kpis.Countries[pos];
+		kpis.Countries.splice(pos, 1);
+	}
+
+	for (let i = 0; i < top10.length; i++) {
+		console.log(top10[i]);
+	}
+
+	var bars = document.getElementById('barras');
+	var myBarsChart = new Chart(bars, {
+		type: 'bar',
+		data: {
+			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true
 				}
 			}
 		}
