ba0a22cb307d7435d3261194986fb5e4c41b51c7
Ricardo Caldeira <ricardocaldeira.zetta@ufla.br>
2021-08-13 22:24:21 -0300

    Adicionado grafico de pizza a pagina home

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,5 +18,44 @@ function loadKpis(kpis) {
 
 	const totalRecuperados = document.getElementById("recovered");
 	totalRecuperados.innerText = kpis.Global.TotalRecovered.toLocaleString("PT");
+
+	loadPizza(kpis);
 	
+}
+
+function loadPizza(kpis) {
+	var pizza = document.getElementById('pizza');
+	var myChart = new Chart(pizza, {
+		type: 'pie',
+		data: {
+			labels: ['Confirmados', 'Recuperados', 'Mortes'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132)',//red
+					'rgba(54, 162, 235)',//blue
+					'rgba(255, 206, 86)'//yellow
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			responsive: true,
+			plugins: {
+				legend: {
+					position: 'top'
+				},
+				title: {
+					display: true,
+					text: "Distribuição de novos casos"
+				}
+			}
+		}
+	});
 }
