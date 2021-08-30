bab3d71db919ea40de67d81d94063d1ca2a25510
Ricardo Caldeira <ricardocaldeira.zetta@ufla.br>
2021-08-30 19:59:07 -0300

    :construction: Candidatos sendo mapeados estaticamente com a primeira cidade do select (Componente Candidate pendente de implementaçao)

--- frontend/src/pages/ElectionsPage.jsx
+++ frontend/src/pages/ElectionsPage.jsx
@@ -11,25 +11,30 @@ export default function ElectionsPage() {
   const [election, setElection] = useState([]);
   const [candidates, setCandidates] = useState([]);
   const [selectedCityId, setSelectedCityId] = useState("680df3f5-c5bf-48ae-bda0-5b667fd89197");
+  const [city, setCity] = useState({});
+  const [allElections, setAllElections] = useState([]);
+  const [allCandidates, setAllCandidates] = useState([]);
 
   useEffect(() => {
       async function getData() {
         try {
-          setCities(await apiGetAllCities());
-          
-          const allElection = await apiGetAllElections();
-          setElection(allElection.filter((election) => {return election.cityId === selectedCityId}));
 
-          const allCandidates = await apiGetAllCandidates();
+          setCities(await apiGetAllCities());
+          setCity(cities.filter((c)=> c.id === selectedCityId));
+
+          setAllElections(await apiGetAllElections());
+          const electionAux = allElections.filter((election) => {return election.cityId === selectedCityId})
+          setElection(electionAux.sort((a, b) => a-b));
+
+          setAllCandidates (await apiGetAllCandidates());
           let candidatesAux = [];
           election.forEach(e => {
             candidatesAux.push(allCandidates.filter((cand) => {return e.candidateId === cand.id }))
           });
           setCandidates(candidatesAux);
-          console.log(candidatesAux);
 
         } catch (error) {
-          //setError(error.message);
+          // setError(error.message);
         }
       }
       getData();
@@ -51,7 +56,24 @@ export default function ElectionsPage() {
                   />
               </div>
               <div className="p-4 border m-4">
-
+                <div className="text-center">
+                  <h2 className="font-bold">{`Eleição em ${city[0].name}`}</h2>
+                  <div className="flex flex-row space-x-2 justify-center m-2">
+                    <span className="font-semibold">{`Total de eleitores: `}</span>{city[0].votingPopulation.toLocaleString("pt-BR")}
+                    <span className="font-semibold">{`Abstenção: `}</span>{city[0].absence.toLocaleString("pt-BR")}
+                    <span className="font-semibold">{`Abstenção: `}</span>{city[0].presence.toLocaleString("pt-BR")}
+                  </div>
+                  <span>{candidates.length-1} candidatos</span>
+                  <>
+                    {election.map(el => {
+                      return <Candidate
+                        key={el.id}
+                        votes={el.votes}
+                        candidate={candidates.filter((candidate => candidate.id === el.candidateId))}
+                      />
+                    })}
+                  </>
+                </div>
               </div>
           </main>
       </div>
