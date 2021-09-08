8305c639f5d36209bb92a3deafa0c0e6d6fe34f0
Ricardo Caldeira <ricardocaldeira.zetta@ufla.br>
2021-09-08 08:55:34 -0300

    frontend iniciado

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default App;
