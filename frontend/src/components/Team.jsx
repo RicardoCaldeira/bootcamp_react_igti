b88f43f71af20b144a94f3d88d45952f6a830751
Ricardo Caldeira <ricardocaldeira.zetta@ufla.br>
2021-10-05 19:27:11 -0300

    finalizada implementação do desafio final

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -4,13 +4,13 @@ export default function Team({pos, team}) {
 			<td>{pos}</td>
 			<td><img src={`../../img/${team.name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(" ", "_")}.png`} width="25px"/></td>
 			<td>{team.name}</td>
-			<td>81</td>
-			<td>28</td>
-			<td>7</td>
-			<td>5</td>
-			<td>80</td>
-			<td>28</td>
-			<td>52</td>
+			<td>{team.pontuacao_geral.total_pontos}</td>
+			<td>{team.pontuacao_geral.total_vitorias}</td>
+			<td>{team.pontuacao_geral.total_empates}</td>
+			<td>{team.pontuacao_geral.total_derrotas}</td>
+			<td>{team.pontuacao_geral.total_gols_marcados}</td>
+			<td>{team.pontuacao_geral.total_gols_sofridos}</td>
+			<td>{team.pontuacao_geral.saldo}</td>
 		</>
 	)
 }
